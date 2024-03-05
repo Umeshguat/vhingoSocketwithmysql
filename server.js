75879341aa73cac6bece48e43352d4cadf249628
Umeshguat <92910458+Umeshguat@users.noreply.github.com>
2024-03-05 10:54:54 +0530

    update on 5 mar

--- server.js
+++ server.js
@@ -44,24 +44,47 @@ io.on('connection', (socket) => {
     })
     socket.emit("room connected", "Connection Successfully");
 
+
+
+
+    //update code for 
     socket.on("tracking", async (btnKaMsg) => {
         const room = btnKaMsg.room;
         const longitude = btnKaMsg.location.longitude;
         const latitude = btnKaMsg.location.latitude;
-        // console.log(latitude);
+    
+        try {
+            const updateQuery = "UPDATE vendorlocations SET latitude = ?, longitude = ? WHERE vendor_id = ?";
+            const result = await query(updateQuery, [latitude, longitude, room]);
+            console.log("result update successfully");
+        } catch(e) {
+            console.log("error", e);
+        }
+    
         socket.join(room);
         socket.emit("room connected", "Ho gaya room connection");
         console.log("location", btnKaMsg);
-        socket.to(room).emit("message recieved", btnKaMsg);
+        socket.to(room).emit("message received", btnKaMsg);
         io.to(room).emit("track location", btnKaMsg);
-        try{
-            const updateQuery = `UPDATE vendorlocations SET latitude = ${latitude}, longitude = ${longitude} WHERE vendor_id = ${room}`;
-            const result = await query(updateQuery);
-            console.log("result update succeessfully");
-        }catch(e){
-            console.log("error",e);
-        }
-    })
+    });
+
+    // socket.on("tracking", async (btnKaMsg) => {
+    //     const room = btnKaMsg.room;
+    //     const longitude = btnKaMsg.location.longitude;
+    //     const latitude = btnKaMsg.location.latitude;
+    //     socket.join(room);
+    //     socket.emit("room connected", "Ho gaya room connection");
+    //     console.log("location", btnKaMsg);
+    //     socket.to(room).emit("message recieved", btnKaMsg);
+    //     io.to(room).emit("track location", btnKaMsg);
+    //     try{
+    //         const updateQuery = `UPDATE vendorlocations SET latitude = ${latitude}, longitude = ${longitude} WHERE vendor_id = ${room}`;
+    //         const result = await query(updateQuery);
+    //         console.log("result update succeessfully");
+    //     }catch(e){
+    //         console.log("error",e);
+    //     }
+    // })
     socket.on('disconnect', (socket) => {
         console.log('Disconnect');
     });
